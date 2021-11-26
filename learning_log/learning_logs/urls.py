8d8c6d83e7218a7556ef9fd6fb5ceecadebc0583
tensor-zhang <15009202810@163.com>
2021-11-27 00:50:42 +0800

    add topics webpage

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,6 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #homepaged
+    #homepage
     path(r'', views.index, name='index'),
+    
+    #topic
+    path(r'^topics/$', views.topics, name='topics'),
+    
+    #special topic
+    path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
