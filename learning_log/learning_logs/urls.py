dfcb2227e5ced3e93b91b60d82de95c7f2357c93
tensor-zhang <15009202810@163.com>
2021-11-29 21:19:13 +0800

    updating for edit entry

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
     
     #webpage for adding new item for topic
     path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    
+    #edit existed item
+    path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -56,4 +56,21 @@ def new_entry(request, topic_id):
             
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    '''editing existed item'''
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
     
+    if request.method != 'POST':
+        #request for firstime, using present entry to fill form
+        form = EntryForm(instance=entry)
+    else:
+        #post data: process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+    
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
